670c59ac12c2b65c8e87f2e2f1aeeb26f2614c1b
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-02-06 13:46:14 -0500

    write back stage with register commit at end

--- processor.cpp
+++ processor.cpp
@@ -6,12 +6,13 @@ using namespace std;
 #ifdef ENABLE_DEBUG
 #define DEBUG(x) x
 #else
-#define DEBUG(x) 
+#define DEBUG(x)
 #endif
 
-void Processor::initialize(int level) {
+void Processor::initialize(int level)
+{
     // Initialize Control
-    control = {.reg_dest = 0, 
+    control = {.reg_dest = 0,
                .jump = 0,
                .jump_reg = 0,
                .link = 0,
@@ -27,35 +28,41 @@ void Processor::initialize(int level) {
                .ALU_src = 0,
                .reg_write = 0,
                .zero_extend = 0};
-   
+
     opt_level = level;
     // Optimization level-specific initialization
 }
 
-void Processor::advance() {
-    switch (opt_level) {
-        case 0: single_cycle_processor_advance();
-                break;
-        case 1: pipelined_processor_advance();
-                break;
-        // other optimization levels go here
-        default: break;
+void Processor::advance()
+{
+    switch (opt_level)
+    {
+    case 0:
+        single_cycle_processor_advance();
+        break;
+    case 1:
+        pipelined_processor_advance();
+        break;
+    // other optimization levels go here
+    default:
+        break;
     }
 }
 
-void Processor::single_cycle_processor_advance() {
+void Processor::single_cycle_processor_advance()
+{
     // fetch
     uint32_t instruction;
     memory->access(regfile.pc, instruction, 0, 1, 0);
     DEBUG(cout << "\nPC: 0x" << std::hex << regfile.pc << std::dec << "\n");
     // increment pc
     regfile.pc += 4;
-    
+
     // decode into contol signals
     control.decode(instruction);
     DEBUG(control.print());
 
-    // extract rs, rt, rd, imm, funct 
+    // extract rs, rt, rd, imm, funct
     int opcode = (instruction >> 26) & 0x3f;
     int rs = (instruction >> 21) & 0x1f;
     int rt = (instruction >> 16) & 0x1f;
@@ -67,17 +74,18 @@ void Processor::single_cycle_processor_advance() {
     // Variables to read data into
     uint32_t read_data_1 = 0;
     uint32_t read_data_2 = 0;
-    
+
     // Read from reg file
     regfile.access(rs, rt, read_data_1, read_data_2, 0, 0, 0);
-    
-    // Execution 
+
+    // Execution
     alu.generate_control_inputs(control.ALU_op, funct, opcode);
-   
+
     // Sign Extend Or Zero Extend the immediate
-    // Using Arithmetic right shift in order to replicate 1 
-    imm = control.zero_extend ? imm : (imm >> 15) ? 0xffff0000 | imm : imm;
-    
+    // Using Arithmetic right shift in order to replicate 1
+    imm = control.zero_extend ? imm : (imm >> 15) ? 0xffff0000 | imm
+                                                  : imm;
+
     // Find operands for the ALU Execution
     // Operand 1 is always R[rs] -> read_data_1, except sll and srl
     // Operand 2 is immediate if ALU_src = 1, for I-type
@@ -86,8 +94,7 @@ void Processor::single_cycle_processor_advance() {
     uint32_t alu_zero = 0;
 
     uint32_t alu_result = alu.execute(operand_1, operand_2, alu_zero);
-    
-    
+
     uint32_t read_data_mem = 0;
     uint32_t write_data_mem = 0;
 
@@ -95,26 +102,31 @@ void Processor::single_cycle_processor_advance() {
     // First read no matter whether it is a load or a store
     memory->access(alu_result, read_data_mem, 0, control.mem_read | control.mem_write, 0);
     // Stores: sb or sh mask and preserve original leftmost bits
-    write_data_mem = control.halfword ? (read_data_mem & 0xffff0000) | (read_data_2 & 0xffff) : 
-                    control.byte ? (read_data_mem & 0xffffff00) | (read_data_2 & 0xff): read_data_2;
+    write_data_mem = control.halfword ? (read_data_mem & 0xffff0000) | (read_data_2 & 0xffff) : control.byte ? (read_data_mem & 0xffffff00) | (read_data_2 & 0xff)
+                                                                                                             : read_data_2;
     // Write to memory only if mem_write is 1, i.e store
     memory->access(alu_result, read_data_mem, write_data_mem, control.mem_read, control.mem_write);
     // Loads: lbu or lhu modify read data by masking
-    read_data_mem &= control.halfword ? 0xffff : control.byte ? 0xff : 0xffffffff;
+    read_data_mem &= control.halfword ? 0xffff : control.byte ? 0xff
+                                                              : 0xffffffff;
 
-    int write_reg = control.link ? 31 : control.reg_dest ? rd : rt;
+    int write_reg = control.link ? 31 : control.reg_dest ? rd
+                                                         : rt;
 
-    uint32_t write_data = control.link ? regfile.pc+8 : control.mem_to_reg ? read_data_mem : alu_result;  
+    uint32_t write_data = control.link ? regfile.pc + 8 : control.mem_to_reg ? read_data_mem
+                                                                             : alu_result;
 
     // Write Back
     regfile.access(0, 0, read_data_2, read_data_2, write_reg, control.reg_write, write_data);
-    
+
     // Update PC
-    regfile.pc += (control.branch && !control.bne && alu_zero) || (control.bne && !alu_zero) ? imm << 2 : 0; 
-    regfile.pc = control.jump_reg ? read_data_1 : control.jump ? (regfile.pc & 0xf0000000) & (addr << 2): regfile.pc;
+    regfile.pc += (control.branch && !control.bne && alu_zero) || (control.bne && !alu_zero) ? imm << 2 : 0;
+    regfile.pc = control.jump_reg ? read_data_1 : control.jump ? (regfile.pc & 0xf0000000) & (addr << 2)
+                                                               : regfile.pc;
 }
 
-void Processor::pipelined_processor_advance() {
+void Processor::pipelined_processor_advance()
+{
     // pipelined processor logic goes here
     // does nothing currently -- if you call it from the cmd line, you'll run into an infinite loop
     // might be helpful to implement stages in a separate module
@@ -123,6 +135,17 @@ void Processor::pipelined_processor_advance() {
     ID_EX_Reg id_ex_reg;
     IF_ID_Reg if_id_reg;
 
-    
+    // write back stage
+    if (mem_wb_reg.control.reg_write)
+    {
+        uint32_t write_data = mem_wb_reg.control.mem_to_reg ? mem_wb_reg.mem_read_data : mem_wb_reg.alu_result;
+        regfile.access(0, 0, write_data, write_data,
+                       mem_wb_reg.write_reg, true, write_data);
+    }
 
+    // commit pipeline registers
+    // mem_wb_reg = new_mem_wb;
+    // ex_mem_reg = new_ex_mem;
+    // id_ex_reg = new_id_ex;
+    // if_id_reg = new_if_id;
 }

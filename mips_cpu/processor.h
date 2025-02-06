0a48e3dcdd045aaf4acfa354e78764b338957982
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-02-06 13:30:36 -0500

    add pipeline registers

--- mips_cpu/processor.h
+++ mips_cpu/processor.h
@@ -2,6 +2,37 @@
 #include "regfile.h"
 #include "ALU.h"
 #include "control.h"
+
+
+// 
+struct IF_ID_Reg {
+    uint32_t instruction;
+    uint32_t pc;
+};
+struct ID_EX_Reg {
+    control_t control;
+    uint32_t pc;
+    uint32_t read_data_1;
+    uint32_t read_data_2;
+    uint32_t immediate;
+    int rs, rt, rd, shamt, funct;
+};
+
+struct EX_MEM_Reg {
+    control_t control;
+    uint32_t alu_result;
+    uint32_t read_data_2; 
+    int write_reg;
+};
+
+struct MEM_WB_Reg {
+    control_t control;
+    uint32_t mem_read_data;
+    uint32_t alu_result;
+    int write_reg;
+};
+
+
 class Processor {
     private:
         int opt_level;
